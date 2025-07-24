da4c71252514cfce4c4f912e72694075efb55159
Seth Margolin <margolin.s@northeastern.edu>
2025-07-24 02:49:52 -0400

    Placeholder implementation of GTP::start() for testing purposes

--- src/gtp.rs
+++ src/gtp.rs
@@ -25,11 +25,30 @@ enum GtpResponse {
 \*****************************************************/
 
 impl GtpResponse {
+    // Writes the result of this GtpResponse to the Go Text Protocol
     fn write_to_gtp(self) {
         match self {
             GtpResponse::SUCCESS(result) => println!("= {result}"),
             GtpResponse::ERROR(result) => println!("? {result}"),
             GtpResponse::DEBUG(result) => eprintln!("{result}"),
         }
+    }
+}
+
+impl GTP {
+    // Starts a Go Text Protocol listener for the Go Engine
+    pub(crate) fn start() -> io::Result<()> {
+        use std::io;
+        let mut buffer = String::new();
+        loop {
+            buffer.clear();
+            io::stdin().read_line(&mut buffer)?;
+            if buffer.eq_ignore_ascii_case("quit\r\n") {
+                break;
+            }
+            println!("{}", buffer);
+        }
+
+        Ok(())
     }
 }
