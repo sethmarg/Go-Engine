bf635b7e49dda8e062c665ed3aa206f740b671ae
Seth Margolin <margolin.s@northeastern.edu>
2025-07-24 13:42:51 -0400

    Fixed a bug where write_to_gtp would not write the result String with two trailing \n

--- src/gtp.rs
+++ src/gtp.rs
@@ -28,9 +28,18 @@ impl GtpResponse {
     // Writes the result of this GtpResponse to the Go Text Protocol
     fn write_to_gtp(self) {
         match self {
-            GtpResponse::SUCCESS(result) => println!("= {result}"),
-            GtpResponse::ERROR(result) => println!("? {result}"),
-            GtpResponse::DEBUG(result) => eprintln!("{result}"),
+            GtpResponse::SUCCESS(result) => println!("= {}", Self::format_gtp_string(result)),
+            GtpResponse::ERROR(result) => println!("? {}", Self::format_gtp_string(result)),
+            GtpResponse::DEBUG(result) => eprintln!("{}", Self::format_gtp_string(result)),
+        }
+    }
+    
+    // Formats the given String such that it conforms to the Go Text Protocol
+    fn format_gtp_string(input: String) -> String {
+        if input.ends_with("\n\n") {
+            input
+        } else {
+            Self::format_gtp_string(format!("{input}\n"))
         }
     }
 }
