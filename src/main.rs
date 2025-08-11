9260825a05b0e47a5b3cf81a44c94e567bf858ef
Seth Margolin <margolin.s@northeastern.edu>
2025-08-11 01:42:31 -0400

    Implemented genmove command

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod tests;
 mod engine;
 
 use board::*;
+use engine::*;
 /****************************************************\
 |****************        MAIN        ****************|
 \****************************************************/
@@ -11,25 +12,8 @@ use board::*;
 fn debug() {
     use ColumnIdentifier::*;
     let mut b: Board = Board::new(BoardSize::NINETEEN);
-    b.play(Move::MOVE(Intersection::new(B, 2), Color::WHITE));
-    b.play(Move::MOVE(Intersection::new(B, 3), Color::WHITE));
-    b.play(Move::MOVE(Intersection::new(C, 2), Color::WHITE));
-    b.play(Move::MOVE(Intersection::new(C, 3), Color::WHITE));
-    // b.position[Intersection { column: B, row: 2 }.to_position_index(&b.size) as usize] =
-    //     State::OCCUPIED(Color::WHITE);
-    // b.position[Intersection { column: B, row: 3 }.to_position_index(&b.size) as usize] =
-    //     State::OCCUPIED(Color::WHITE);
-    // b.position[Intersection { column: C, row: 2 }.to_position_index(&b.size) as usize] =
-    //     State::OCCUPIED(Color::WHITE);
-    // b.position[Intersection { column: C, row: 3 }.to_position_index(&b.size) as usize] =
-    //     State::OCCUPIED(Color::WHITE);
-    print!("{}", b.to_string());
-    let (group, liberties) = b.count(
-        Intersection::new(B, 2).to_position_index(&b.size).unwrap() as usize,
-        Color::WHITE,
-    );
-    println!("Group: {:#?}", group);
-    println!("Liberties: {:#?}", liberties);
+    
+    println!("{:?}", generate_move(&b, Color::BLACK, 30));
 }
 
 fn main() {
