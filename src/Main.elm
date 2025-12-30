6a8e6655f94e12aa383ea09db955d2f483f1bdae
Seth Margolin <margolin.s@northeastern.edu>
2025-12-30 03:17:46 -0500

    Added current player to Model

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,9 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div)
+import Html exposing (Html, div, img)
+import Html.Attributes exposing (src, style)
+import Html.Events exposing (onClick)
 
 main : Program () Model Msg
 main = Browser.sandbox
@@ -19,7 +21,8 @@ type alias State = Maybe Color
 type alias Board = List (List State)
 
 type alias Model = 
-    { board : Board}
+    { board : Board 
+    , curPlayer : Color }
 
 type Msg 
     = Play Position
@@ -30,13 +33,23 @@ type alias Position = { row : Int, col : Int}
 
 init : Model
 init = 
-    { board = List.repeat 19 (List.repeat 19 Nothing)}
+    { board = List.repeat 19 (List.repeat 19 (Just White))
+    , curPlayer = Black}
 
 update : Msg -> Model -> Model
 update msg model = 
     case msg of 
         Play pos ->
-            model
+            { model |
+                board = indexedMap2d 
+                    (\x -> \y -> \state -> 
+                        if x == pos.row && y == pos.col then
+                            Just model.curPlayer
+                        else 
+                            state)
+                    model.board,
+                curPlayer = oppositeColor model.curPlayer
+                }
         Pass ->
             model
         Resign ->
@@ -44,8 +57,57 @@ update msg model =
 
 view : Model -> Html Msg
 view model = 
-    div [] []
+    --div [] [img [src "Go Board.png"] []]
+    --img [src "Go Board.png"] (List.map (div []) (map2d stateToImage model.board))
+    --div [] (List.map (div []) (map2d stateToImage model.board))
+    --div [style "position" "relative"] (img [src "Go Board.png"] [] :: (List.map (div [style "position" "relative"]) (map2d stateToImage model.board)))
+    div [] (img [src "Go Board.png"] [] :: (List.map (div [style "position" "relative"]) (indexedMap2d indexedStateToImage model.board)))
 
 map2d : (a -> b) -> List (List a) -> List (List b)
 map2d fn list = 
     List.map (List.map fn) list
+
+indexedMap2d : (Int -> Int -> a -> b) -> List (List a) -> List (List b)
+indexedMap2d fn list =
+    List.indexedMap
+        (\x -> \column -> 
+            List.indexedMap
+                (\y -> \elem ->
+                    fn x y elem)
+                column
+        )
+        list
+
+colorToImage : Color -> Html Msg
+colorToImage color =
+    case color of 
+        Black -> 
+            img [src "black.png"] []
+        White -> 
+            img [src "white.png"] []
+
+indexedStateToImage : Int -> Int -> State -> Html Msg
+indexedStateToImage x y state =
+    div 
+        [ onClick (Play (Position x y)) 
+        , style "position" "relative"] 
+        [ case state of 
+            Just color -> 
+                colorToImage color
+            Nothing -> 
+                div [] []
+        ]
+
+stateToImage : State -> Html Msg
+stateToImage state = 
+  case state of 
+    Just color -> 
+        colorToImage color
+    Nothing -> 
+        div [] []
+        
+oppositeColor : Color -> Color
+oppositeColor color =
+    case color of
+        White -> Black
+        Black -> White
